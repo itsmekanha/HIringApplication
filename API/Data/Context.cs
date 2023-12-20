32623fdff77e3e0752c9e82a27f0cb6276b3326c
Shrikrushna <dev1@flairminds.com>
2023-12-20 17:55:06 +0530

    Commit starting of project

---
+++ API/Data/Context.cs
@@ -0,0 +1,14 @@
+﻿using API.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class Context : IdentityDbContext<user>
+    {
+        public Context(DbContextOptions<Context> options) : base(options) 
+        {
+
+        }
+    }
+}
---
+++ API/Models/user.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Models
+{
+    public class user : IdentityUser
+    {
+        [Required]
+        public string FirstName {  get; set; }
+        public string LastName { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.UtcNow;
+
+    }
+}
